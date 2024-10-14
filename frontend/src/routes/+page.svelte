04c4a5d896fd816c7965d2aec1a6b059cfaa0304
Kiliups <kilian.mayer@st.oth-regensburg.de>
2024-10-14 20:58:49 +0200

    init

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	//imports
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Hello world" />
+</svelte:head>
+
+<section>
+	<h1>
+		hello world
+	</h1>
+</section>
